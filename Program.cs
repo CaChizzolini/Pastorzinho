b449a968d6f43d90770c87c417c0a17171e50b5c
CaChizzolini <camilli.chizzolini@gmail.com>
2023-08-18 00:41:21 -0300

    concluído

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿//Camilli Chizzolini 
+
+//"Dó, ré, mi, fá, fá, fá"
+Console.Clear();
+Console.Write("Dó, ");
+Console.Beep(1320, 200);
+Console.Write("ré, ");
+Console.Beep(1485, 200);
+Console.Write("mi, ");
+Console.Beep(1650, 200);
+Console.Write("fá, ");
+Console.Beep(1759, 200);
+
+Thread.Sleep(200);
+
+Console.Write("fá, ");
+Console.Beep(1759, 200);
+Console.WriteLine("fá,");
+Console.Beep(1759, 200);
+
+Thread.Sleep(500);
+
+//"Dó, ré, dó, ré, ré, ré"
+Console.Write("Dó, ");
+Console.Beep(1320, 200);
+Console.Write("ré, ");
+Console.Beep(1485, 200);
+Console.Write("dó, ");
+Console.Beep(1320, 200);
+Console.Write("ré, ");
+Console.Beep(1485, 200);
+
+Thread.Sleep(300);
+
+Console.Write("ré, ");
+Console.Beep(1485, 200);
+Console.WriteLine("ré,");
+Console.Beep(1485, 200);
+
+Thread.Sleep(500);
+
+//"Dó, sol, fá, mi, mi, mi"
+Console.Write("Dó, ");
+Console.Beep(1320, 200);
+Console.Write("sol, ");
+Console.Beep(1980, 200);
+Console.Write("fá, ");
+Console.Beep(1759, 200);
+Console.Write("mi, ");
+Console.Beep(1650, 200);
+
+Thread.Sleep(300);
+
+Console.Write("mi, ");
+Console.Beep(1650, 200);
+Console.WriteLine("mi,");
+Console.Beep(1650, 200);
+
+Thread.Sleep(500);
+
+//"Dó, ré, mi, fá, fá, fá."
+Console.Write("Dó, ");
+Console.Beep(1320, 200);
+Console.Write("ré, ");
+Console.Beep(1485, 200);
+Console.Write("mi, ");
+Console.Beep(1650, 200);
+Console.Write("fá, ");
+Console.Beep(1759, 200);
+
+Thread.Sleep(300);
+
+Console.Write("fá, ");
+Console.Beep(1759, 200);
+Console.Write("fá.");
+Console.Beep(1759, 200);
+
